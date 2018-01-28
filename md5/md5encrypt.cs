49d055c47e9ddd749f2a6e550e9c87a40875f8ac
ErlandD <erlanddevona@gmail.com>
2018-01-28 14:18:35 +0700

    Add files via upload

---
+++ md5/md5encrypt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace md5
+{
+    public class md5encrypt
+    {
+        public static String encrypt(String input)
+        {
+            MD5 hash = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashCompute = hash.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashCompute.Length; i++) { sb.Append(hashCompute[i].ToString("X2")); }
+            return sb.ToString();
+        }
+    }
+}
